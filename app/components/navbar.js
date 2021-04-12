25ac2a69773b4738f550423609a8468e62ac4aab
veikkosalminen <veikko.salminen@gmail.com>
2021-04-12 13:04:05 +0000

    working menu

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,8 +1,11 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object'
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
 
 export default class NavbarComponent extends Component {
+
+    @service router
 
     @tracked showMenu = false
 
---
+++ app/routes/page.js
@@ -0,0 +1,9 @@
+import Route from '@ember/routing/route';
+
+export default class PageRoute extends Route {
+
+    model(params) {
+        return params
+    }
+
+}
