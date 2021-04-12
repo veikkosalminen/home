3e8065f3e2845484ee7187577f3d88b7fab9b16a
veikkosalminen <veikko.salminen@gmail.com>
2021-04-12 13:46:44 +0000

    save config to localstorage

--- app/services/backend.js
+++ app/services/backend.js
@@ -5,12 +5,26 @@ export default class BackendService extends Service {
     url = "https://script.google.com/macros/s/AKfycbzmIfcn1upgDsZEWKuaoJHuqSdRj0wdT0c8km_o3ejsV3hK9ZM/exec"
 
     async getPages() {
-        const resp = await fetch(this.url + "?sheet=config")
-        const data = await resp.json()
+
+        const config = localStorage.getItem("config")
+        let json
+        const fetchConfig = fetch(this.url + "?sheet=config")
+
+        if (config) {
+            fetchConfig.then(response => response.json())
+            .then(data => localStorage.setItem("config", JSON.stringify(data)))
+
+            json = JSON.parse(config)
+        }
+        else {
+            const resp = await fetchConfig
+            json = await resp.json()
+            localStorage.setItem("config", JSON.stringify(json))
+        }
         
         let currentPage = null
         const pages = {}
-        data.config.forEach((row) => {
+        json.config.forEach((row) => {
             if (row.page.startsWith("#")) return
             if (row.page) currentPage = row.page
             if (!pages[currentPage]) pages[currentPage] = []
