d2315d6acacfa5bcc20b8e372b51e2f4dfadba99
veikkosalminen <veikko.salminen@gmail.com>
2021-04-12 10:26:52 +0000

    pwa plugin

---
+++ config/manifest.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = function(/* environment, appConfig */) {
+  // See https://zonkyio.github.io/ember-web-app for a list of
+  // supported properties
+
+  return {
+    name: "Home",
+    short_name: "home",
+    description: "",
+    start_url: "/",
+    scope: "/",
+    display: "standalone",
+    background_color: "#000",
+    theme_color: "#000",
+    icons: [{
+      "src": "https://cdn.iconscout.com/icon/free/png-256/home-803-450320.png",
+      "sizes": "256x256",
+      "type": "image/png"
+    }
+    ],
+    ms: {
+      tileColor: '#000'
+    }
+  };
+}
